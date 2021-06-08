2eedc7246fe0420093bf871d6ca8db4db01683ff
DifenYedidya <difen.yedidya@gmail.com>
2021-06-08 20:23:14 +0700

    our team & scroll up added

---
+++ script.js
@@ -0,0 +1,16 @@
+let topButton = document.getElementById("topBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      topButton.style.display = "block";
+    } else {
+      topButton.style.display = "none";
+    }
+  }
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
